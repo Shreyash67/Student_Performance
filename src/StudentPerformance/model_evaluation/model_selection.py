30348f7aa85eda4f4294eae11cf49bc1504238c6
SHREYASH WARALKAR <135358294+Shreyash67@users.noreply.github.com>
2025-04-14 00:54:29 +0530

    Adding model_selection.py

--- src/StudentPerformance/model_evaluation/model_selection.py
+++ src/StudentPerformance/model_evaluation/model_selection.py
@@ -1,8 +1,28 @@
-# import pandas as pd
-# from src.StudentPerformance.train_test.train_model import train_model
-# from sklearn.linear_model import LogisticRegression
+import pandas as pd
+from src.StudentPerformance.train_test.train_model import train_model
+from sklearn.linear_model import LogisticRegression
 
-# def model_selection(df):
+def model_selection(df):
+    x_train,x_test,y_train,y_test = train_model(df)
+
+    model = LogisticRegression()
+
+    model.fit(x_train,y_train)
+
+    y_pred = model.predict(x_test)
+
+    return y_pred
+
+def actual_model(df):
+    x_train,x_test,y_train,y_test = train_model(df)
+
+    model = LogisticRegression()
+
+    model.fit(x_train,y_train)
+
+    model_name = model.__class__.__name__
+
+    return model_name,model
 
     
 
