a9f354d277d3320c705ba9c8fcb135c212b1d264
SHREYASH WARALKAR <135358294+Shreyash67@users.noreply.github.com>
2025-04-26 00:11:47 +0530

    Modified data.csv

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
 from flask import Flask,render_template,request
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+
+with open('H:\Student_Performance\data\StudentsPerformance.csv','rb') as file:
+    model = pickle.load(file)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict',methods=['GET','POST'])
+def predict():
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        pass
